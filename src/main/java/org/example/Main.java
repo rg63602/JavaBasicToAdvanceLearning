31c5191002e71aac7fa1f37b607fb6efff9f51ae
rg63602 <30683886+rg63602@users.noreply.github.com>
2024-08-23 23:10:06 +0530

    java-basic-to-advance/java-fundamental : JVM & Bytecode learning

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Student student = new Student(123, "Rishabh Kumar");
+        System.out.println("Hello world!" + student.getName() + " " + student.getId());
     }
 }
