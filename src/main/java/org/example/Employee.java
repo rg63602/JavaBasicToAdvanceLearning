4c522809f34b045cce5cf6b1de621abf54158e6c
rg63602 <30683886+rg63602@users.noreply.github.com>
2024-08-25 23:26:39 +0530

    java-basic-to-advance/java-fundamental : Java JDK & Java Variable Concept

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Employee {
+    Byte aByte;
+
+    public Byte getaByte() {
+        byte localVariable = 5;
+        System.out.println("local variable : " + localVariable);
+        return aByte;
+    }
+
+    public static void main(String[] args) {
+        int a = -10;
+        System.out.println("This is my first program and output of a is " + a);
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Student student = new Student(123, "Rishabh Kumar");
         System.out.println("Hello world!" + student.getName() + " " + student.getId());
+        Employee employee = new Employee();
+        System.out.println("default value of Byte : " + employee.getaByte());
     }
 }
