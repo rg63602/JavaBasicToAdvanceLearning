72430a899e4379b910949281572acd8ab5fc9e5b
rg63602 <30683886+rg63602@users.noreply.github.com>
2024-08-31 17:20:16 +0530

    Merge pull request #2 from rg63602/java-basic-to-advance/java-fundamental
    java-basic-to-advance/java-fundamental : Java Big Integer, Float, Dou…

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Student {
+    private long id;
+
+    public Student(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
